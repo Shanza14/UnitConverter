cd41a6f34c8b4c2fd6aff65b98d5abf9bd977877
Shanza14 <126158332+Shanza14@users.noreply.github.com>
2023-05-30 14:53:50 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value
+    
+    lengthEl.textContent = `${baseValue} meter = ${(baseValue * meterToFeet).toFixed(2)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(2)} meters` 
+    
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(2)} gallon | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(2)} liters`
+    
+    massEl.textContent = `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(2)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(2)} kilos`
+})
+
